e107d722ed8ed69af4baa99101ad9decc5cb8542
augustshot <ulyana.yamanova@yandex.ru>
2025-03-29 18:32:02 +0800

    ex. 3 input fix

--- js/third.js
+++ js/third.js
@@ -6,44 +6,86 @@
         document.getElementById("res").disabled = true; 
         document.getElementById("num").disabled = true; 
         document.addEventListener("keyup", enterUp);
-        document.getElementById("zero").addEventListener("input", binprov);
-        document.getElementById("one").addEventListener("input", binprov);
-        document.getElementById("num").addEventListener("input", provarg);
+        document.getElementById("zero").addEventListener("input", inputCheck);
+        document.getElementById("one").addEventListener("input", inputCheck);
+        document.getElementById("num").addEventListener("input", inputCheck);
     }
     function enterUp(event) {
         if(document.getElementById("res").disabled == true) return 0;
         if (event.code == "Enter") click();
     }
     
-    function binprov(){
-        if (this.value=='') document.getElementById("num").disabled = true; 
-        else  document.getElementById("num").disabled = false; 
-        const message = document.getElementById('message');
-        this.value=this.value.replace(/[^0-1]/g,"");
-        let length = this.value.length;
-        if(document.getElementById("zero").value.length!=document.getElementById("one").value.length)
-            { 
-                message.textContent = 'Длина остаточных должна быть одинаковая';
-                document.getElementById('num').disabled = true;
-            } 
-        else 
-        {
-            message.textContent = '';
-            document.getElementById('num').disabled = false;
-            if (length > 0 && (length & (length - 1)) === 0) {
-                message.textContent = '';
-                document.getElementById('num').disabled = false;
-            } else {
-                message.textContent = 'Длина не является степенью двойки';
-                document.getElementById('num').disabled = true;
-            }
+    function inputCheck(){
+        if(binprov("zero") == 0 | binprov("one") == 0 | provarg() == 0 | isEqualLength() == 0){
+            document.getElementById("res").disabled = true;
+            // console.log(binprov(), prov(), provarg())
         }
+        else document.getElementById("res").disabled = false;
+    }
+
+    function isEqualLength(){
+        if(document.getElementById("zero").value.length!=document.getElementById("one").value.length){ 
+                message.textContent = 'Длина остаточных должна быть одинаковой';
+                document.getElementById('num').disabled = true;
+                return 0;
+            } 
+        return 1;
+    }
+
+    function binprov(id){
+        const message = document.getElementById('message');
+        var vector = document.getElementById(id);
+        console.log(id);
+        vector.value=vector.value.replace(/[^0-1]/g,"");
+        let length = vector.value.length;
+        if(length == 1 || length == 0){
+            message.textContent = 'Слишком короткий вектор!';
+            document.getElementById("num").disabled = true;
+            return 0;
+        }
+        
+        if ((length & (length - 1)) === 0) {
+            message.textContent = '';
+            document.getElementById("num").disabled = false;
+            return 1;
+        } 
+        message.textContent = 'Длина не является степенью двойки';
+        document.getElementById("num").disabled = true;
+        return 0;
+
+        // if (this.value=='') document.getElementById("num").disabled = true; 
+        // else  document.getElementById("num").disabled = false; 
+        // const message = document.getElementById('message');
+        // this.value=this.value.replace(/[^0-1]/g,"");
+        // let length = this.value.length;
+        // if(document.getElementById("zero").value.length!=document.getElementById("one").value.length)
+        //     { 
+        //         message.textContent = 'Длина остаточных должна быть одинаковая';
+        //         document.getElementById('num').disabled = true;
+        //     } 
+        // else 
+        // {
+        //     message.textContent = '';
+        //     document.getElementById('num').disabled = false;
+        //     if (length > 0 && (length & (length - 1)) === 0) {
+        //         message.textContent = '';
+        //         document.getElementById('num').disabled = false;
+        //     } else {
+        //         message.textContent = 'Длина не является степенью двойки';
+        //         document.getElementById('num').disabled = true;
+        //     }
+        // }
     }
     function provarg(){
-        if (this.value=='') document.getElementById("res").disabled = true; 
-        else  document.getElementById("res").disabled = false; 
-        this.value = this.value.replace(/[^\d]/g, "");
-        if (this.value>Math.log2(document.getElementById("zero").value.length*2))this.value=Math.log2(document.getElementById("zero").value.length*2);
+        var arg = document.getElementById("num");
+        arg.value = arg.value.replace(/[^\d]/g, "");
+        if (arg.value == '0' || binprov("one") == 0 || binprov("zero") == 0){
+            arg.value='';
+            return 0;
+        }
+        if(arg.value.length == 0) return 0;
+        if (arg.value>Math.log2(document.getElementById("zero").value.length*2))arg.value=Math.log2(document.getElementById("zero").value.length*2);
+        return 1;
     }
 
 
