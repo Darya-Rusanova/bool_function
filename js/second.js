25b389b2124b5d8f2b3df2392e22fbba123aff13
augustshot <ulyana.yamanova@yandex.ru>
2025-03-29 17:50:05 +0800

    ex. 2 input fix

--- js/second.js
+++ js/second.js
@@ -3,46 +3,67 @@
 
     function init() {
         document.getElementById("res").addEventListener("click", click);
-        document.getElementById("residual").disabled = true; 
-        document.getElementById("num").disabled = true; 
         document.getElementById("res").disabled = true; 
+        document.getElementById("num").disabled = true;
         document.addEventListener("keyup", enterUp);
-        document.getElementById("vector").addEventListener("input", binprov);
-        document.getElementById("residual").addEventListener("input", prov);
-        document.getElementById("num").addEventListener("input", provarg);
+        document.getElementById("vector").addEventListener("input", inputCheck);
+        document.getElementById("residual").addEventListener("input", inputCheck);
+        document.getElementById("num").addEventListener("input", inputCheck);
     }
     function enterUp(event) {
         if(document.getElementById("res").disabled == true) return 0;
         if (event.code == "Enter") click();
     }
+
+    function inputCheck(){
+        if(binprov() == 0 | prov() == 0 | provarg() == 0){
+            document.getElementById("res").disabled = true;
+            // console.log(binprov(), prov(), provarg())
+        }
+        else document.getElementById("res").disabled = false;
+    }
+
     function binprov(){
+        // console.log('checking vector');
         const message = document.getElementById('message');
-        this.value=this.value.replace(/[^0-1]/g,"");
-        let length = document.getElementById("vector").value.length;
-            if (length > 0 && (length & (length - 1)) === 0) {
-                message.textContent = '';
-                document.getElementById('residual').disabled = false;
-            } else {
-                message.textContent = 'Длина не является степенью двойки';
-                document.getElementById("residual").disabled = true;
-            }
-        if (this.value=='') document.getElementById("residual").disabled = true; 
-        else  document.getElementById("residual").disabled = false; 
+        var vector = document.getElementById("vector");
+        vector.value=vector.value.replace(/[^0-1]/g,"");
+        let length = vector.value.length;
+        if(length == 1 || length == 0){
+            message.textContent = 'Слишком короткий вектор!';
+            document.getElementById("num").disabled = true;
+            return 0;
+        }
+        if ((length & (length - 1)) === 0) {
+            message.textContent = '';
+            document.getElementById("num").disabled = false;
+            return 1;
+        } 
+        message.textContent = 'Длина не является степенью двойки';
+        document.getElementById("num").disabled = true;
+        return 0;
     }
 
     function prov(){
-        if (this.value=='') document.getElementById("num").disabled = true; 
-        else  document.getElementById("num").disabled = false; 
-        this.value=this.value.replace(/[^0-1]/g,"")
-        if (this.value.length>1)this.value=this.value.substr(0,1);
+        // console.log('checking 0/1');
+        var residual = document.getElementById("residual");
+        residual.value=residual.value.replace(/[^0-1]/g,"")
+        if (residual.value.length>1) residual.value=residual.value.substr(0,1);
+        if(residual.value.length == 0) return 0;
+        return 1;
     }
 
     function provarg(){
-        if (this.value == '0') this.value='';
-        if (this.value=='') document.getElementById("res").disabled = true; 
-        else  document.getElementById("res").disabled = false; 
-        this.value = this.value.replace(/[^\d]/g, "");
-        if (this.value>Math.log2(document.getElementById("vector").value.length))this.value=Math.log2(document.getElementById("vector").value.length);
+        // console.log('checking arg');
+        var arg = document.getElementById("num");
+        arg.value = arg.value.replace(/[^\d]/g, "");
+        if (arg.value == '0' || binprov() == 0){
+            arg.value='';
+            return 0;
+        }
+        if (arg.value>Math.log2(document.getElementById("vector").value.length))arg.value=Math.log2(document.getElementById("vector").value.length);
+        if(arg.value.length == 0) return 0;
+        return 1;
     }
 
     function click() {
